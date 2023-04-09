5e733185aae9d9b640caf0c74e226b8517b9dad4
Obiorah Bryan <106826157+Obiorahbryan@users.noreply.github.com>
2023-04-09 16:12:13 +0100

    done

--- script.js
+++ script.js
@@ -16,3 +16,30 @@ showHide.addEventListener('click', function () {
     showHide.innerHTML = showHide.innerHTML === 'read more...' ? 'show less...' : 'read more...';
     midSec.style.height = midSec.style.height === '1300px' ? '850px' : '1300px';
 });
+
+/*Onload section*/
+const textContainer = document.getElementById("text-container");
+const mainContent = document.getElementById("main-content");
+
+// Define the text to be typed out
+const text = "<Obiorah Bryan/>";
+
+// Define the time to wait before displaying the "Hello World" message
+const waitTime = 3 * 1000; // 3 minutes in milliseconds
+
+// Start typing out the text
+let i = 0;
+const typeInterval = setInterval(() => {
+textContainer.textContent += text[i];
+i++;
+    if (i === text.length) {
+        clearInterval(typeInterval);
+        // Wait for the specified time before displaying the "Hello World" message
+        setTimeout(() => {
+        // Remove the text and background color
+        textContainer.style.display = "none";
+        document.body.style.backgroundColor = "white";
+
+        }, waitTime);
+    }
+}, 100);
